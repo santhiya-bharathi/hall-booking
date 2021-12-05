c3348e1ff907bb0ab921d7619a0c51263c61c9f4
santhiya-bharathi <santhiyabharathi1999@gmail.com>
2021-12-05 14:39:32 +0530

    api added

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const app = express();
-const PORT = 9000;
+const PORT = process.env.PORT;
+
+
+
+app.use(express.json());
 
 const hallbooking = [{
     "id":"10",
@@ -9,10 +17,70 @@ const hallbooking = [{
     "starttime":"09:00 AM",
     "endtime":"06:00 PM",
     "room-id":"A10"
+},{
+    "id":"11",
+    "customername":"ray",
+    "date":"2/10/2021",
+    "starttime":"09:00 AM",
+    "endtime":"05:00 PM",
+    "room-id":"A11"
+},{
+    "id":"12",
+    "customername":"yan",
+    "date":"1/10/2021",
+    "starttime":"10:00 AM",
+    "endtime":"03:00 PM",
+    "room-id":"A12"
+},{
+    "id":"13",
+    "customername":"bee",
+    "date":"3/10/2021",
+    "starttime":"09:00 AM",
+    "endtime":"04:00 PM",
+    "room-id":"A13"
+},{
+    "id":"14",
+    "customername":"rishi",
+    "date":"4/10/2021",
+    "starttime":"11:00 AM",
+    "endtime":"05:00 PM",
+    "room-id":"A14"
+},{
+    "id":"15",
+    "customername":"kirsh",
+    "date":"5/10/2021",
+    "starttime":"10:00 AM",
+    "endtime":"05:00 PM",
+    "room-id":"A15"
+},
+{
+    "id":"16",
+    "customername":"kirth",
+    "date":"6/10/2021",
+    "starttime":"09:00 AM",
+    "endtime":"05:00 PM",
+    "room-id":"A16"
+},
+{
+    "id":"17",
+    "customername":"murg",
+    "date":"1/10/2021",
+    "starttime":"10:00 AM",
+    "endtime":"05:00 PM",
+    "room-id":"A12"
+},
+{
+    "id":"18",
+    "customername":"deep",
+    "date":"4/10/2021",
+    "starttime":"09:00 AM",
+    "endtime":"05:00 PM",
+    "room-id":"A15"
 }];
 
+
 app.get("/",(request,response)=>{
-    response.send("hello world");
+    response.send("for hall booking details api ( /hallbooking )");
 });
 
 app.get("/hallbooking",(request,response)=>{
